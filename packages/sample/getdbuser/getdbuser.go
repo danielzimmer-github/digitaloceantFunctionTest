656a3543b3d26f51402ac27bcfd9dd9ead3d4b81
Daniel_Zimmer <dzimmer15@gmail.com>
2022-11-01 13:30:30 -0700

    path testing and experimentation

---
+++ packages/sample/getdbuser/getdbuser.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+	"fmt"
+)
+
+type Request struct {
+	Name string `json:"name"`
+	Text string `json:"text"`
+}
+
+type Response struct {
+	StatusCode 	int 				`json:"statusCode,omitempty"`
+	Headers 	map[string]string 	`json:"headers,omitempty"`
+	Body 		string 				`json:"body,omitempty"`
+}
+
+func Main(in Request) (*Response, error) {
+
+	return &Response{
+		Body: fmt.Sprint(os.Getenv("user")),
+	}, nil
+}
