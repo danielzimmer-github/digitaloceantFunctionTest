9fa67a7fdca54baf84b292459855db7b78b99d5f
Daniel_Zimmer <dzimmer15@gmail.com>
2022-11-01 11:51:57 -0700

    Database Connections

--- packages/sample/authenticate/authenticate.go
+++ packages/sample/authenticate/authenticate.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	_ "github.com/lib/pq"
 )
+
+type CoolThings struct {
+	Name string `json:"name"`
+	Text string `json:"string"`
+}
 
 type Request struct {
 	Name string `json:"name"`
@@ -14,12 +24,21 @@ type Response struct {
 	Body string 				`json:"body,omitempty"`
 }
 
-func Main(in Request) (*Response, error) {
+
+
+func Main(in CoolThings) (*Response, error) {
 	if in.Name == "" {
 		in.Name = "stranger"
 	}
+	if in.Text == "" {
+		return &Response{
+			Body: fmt.Sprintf("No text submitted");
+		}
+	}
+
+
 
 	return &Response{
-		Body: fmt.Sprintf("Hello %s", in.Name),
+		Body: fmt.Sprintf("Authenticate %s", in.Name),
 	}, nil
 }
